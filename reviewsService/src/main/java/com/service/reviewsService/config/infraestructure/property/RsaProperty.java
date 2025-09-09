1820f85d8bf45a1d7ecd5976e0403c3f9648d5fc
David15Barrera <david1315barrera@gmail.com>
2025-09-09 00:49:43 -0600

    Configuration

---
+++ reviewsService/src/main/java/com/service/reviewsService/config/infraestructure/property/RsaProperty.java
@@ -0,0 +1,12 @@
+package com.service.reviewsService.config.infraestructure.property;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPublicKey;
+
+
+@ConfigurationProperties("security.rsa")
+public record RsaProperty(
+        RSAPublicKey publicKey) {
+}
